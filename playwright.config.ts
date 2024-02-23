deb8de3cbd6fd44cd53b44c0597c329386b0e4f8
Dariusz Hryciuk <you@example.com>
2024-02-23 16:43:35 +0100

    assertions

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig, devices } from '@playwright/test';
+import type {PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
 
 
-export default defineConfig({
+const config: PlaywrightTestConfig = {
   testDir: './tests',
+
+  timeout: 30 * 1000,
+
+  expect: {
+     timeout: 6000,
+    },
  
   fullyParallel: true,
 
@@ -17,22 +24,33 @@ export default defineConfig({
   use: {
     headless: false,
     trace: 'on-first-retry',
+    launchOptions:{
+      slowMo: 1000,
+    },
   },
 
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        headless: false,
+        ...devices['Desktop Chrome'] },
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { 
+        headless: false,
+        ...devices['Desktop Firefox'] },
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: {
+        headless: false,
+        ...devices['Desktop Safari'] },
     },
   ],
-});
+};
+
+export default config;
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,114 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title1', async () => {
+  const browser = await chromium.launch(
+    {
+      headless: false,
+      slowMo: 1000
+    }
+  );
+  const browserInstance = await browser.newContext();
+  const page = await browserInstance.newPage();
+
   await page.goto('http://www.automationpractice.pl/index.php');
+
+  //locator WOMEN category
+  //await page.getByText('Women').first().click();
+
+  //locator DRESSES category - problem
+  //await page.getByTitle('Dresses').first().click();
+
+  //locator Sign in link
+  await page.getByText('Sign in').first().click();
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/My Shop/);
+  await browser.close();
+});
+
+
+//korzystam z page jako fixture od playwright
+test('has title2', async ({page}) => {
+
+  await page.goto('http://www.automationpractice.pl/index.php', { waitUntil: 'domcontentloaded' });
+
+  //locator Sign in link
+  await page.getByText('Sign in').first().click();
+
+  // Expect a title "to contain" a substring.
+  //await expect(page).toHaveTitle(/My Shop/);
+  await expect(page).toHaveTitle("Login - My Shop");
+  await page.close();
+
+  //page loading theory
+  //https://developer.mozilla.org/en-US/docs/Web/API/Document/readyState
+  //w koncoli przeglądarki wpisz document.readyState
+});
+
+
+test('assertions for Tops subcategorie', async ({page}) => {
+
+  await page.goto('http://www.automationpractice.pl/index.php', { waitUntil: 'domcontentloaded' });
+
+  //locator WOMEN category
+  await page.getByText('Women').first().click();
+
+  //check if subcategories exists
+
+  //1. Tops
+  //await expect(page.getByText('Tops')).toBeVisible();  //jest error
+  //element jest nieunikalny, widać na stronie 4 takie elementy. 
+  //Trzeba użyć lepszego lokatora. 
+  await expect(page.getByRole('link', { name: 'Tops', exact: true })).toBeVisible();
+
+  await page.close();
+
+});
+
+test('assertions for all subcategories - PASS', async ({page}) => {
+
+  await page.goto('http://www.automationpractice.pl/index.php', { waitUntil: 'domcontentloaded' });
+
+  //locator WOMEN category
+  await page.getByText('Women').first().click();
+
+  //check if subcategories exists
+
+  //1. Tops
+  await expect(page.getByRole('link', { name: 'Tops', exact: true })).toBeVisible();
+
+  
+  //2. Dresses
+  await expect(page.getByRole('link', { name: 'Dresses', exact: true }).first()).toBeVisible();
+
+
+  await expect(page).toHaveTitle("Women - My Shop");
+  await page.close();
+});
+
+test('assertions for all subcategories - soft assertions', async ({page}) => {
+  //use case
+  //sprawdzamy czy wszystkie subkategorie są widoczne jeśli nie to zwracamy błąd i druga asercja nie jest sprawdzana
+  //rozwiązanie - soft assertions
+ 
+  await page.goto('http://www.automationpractice.pl/index.php', { waitUntil: 'domcontentloaded' });
+
+  //locator WOMEN category
+  await page.getByText('Women').first().click();
+
+  //check if subcategories exists
+
+  //1. Tops  //celowy błąd
+  await expect.soft(page.getByRole('link', { name: 'pierwszy celowy_blad', exact: true })).toBeVisible();
+
+  
+  //2. Dresses
+  await expect.soft(page.getByRole('link', { name: 'drugi celowy blad', exact: true }).first()).toBeVisible();
+
+
+  await expect(page).toHaveTitle("Women - My Shop");
+  await page.close();
+
+  //!!!!! uruchomić test w terminau i zobaczyć wynik
+  //npx playwright test -g "assertions for all subcategories - soft assertions"
 });
